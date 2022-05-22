53265708bd4d1523c24f97999e105a47c71fe16b
Alaa Abdulridha <alaasabrish@gmail.com>
2022-05-22 14:43:14 +0200

    Upload the GMTOOLS

---
+++ Gm_Tool/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Gm_Tool")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("RevolutionTeam")>
+<Assembly: AssemblyProduct("Gm_Tool")>
+<Assembly: AssemblyCopyright("Copyright Shalan K. ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a2f6108c-e42b-4fd0-9b7d-20f1ccd16fc6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.8.0.0")>
+<Assembly: AssemblyFileVersion("1.8.0.0")>
